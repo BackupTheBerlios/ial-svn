c6cbbe0a3daf0ff2378085fb859947d2afe6ccfe
thoenig <thoenig@8e7fc960-65e5-0310-a079-fea189f357c6>
2005-02-15 10:44:28 +0000

    sending input events as array of strings of strings

--- trunk/iald/iald_dbus.c
+++ trunk/iald/iald_dbus.c
@@ -21,3 +21,108 @@
  *
  */
 
+#include "iald.h"
+#include "iald_mod.h"
+#include "iald_dbus.h"
+
+extern GMainLoop *loop;
+extern DBusConnection *dbus_connection;
+extern IalModule *modules_list_head;
+
+DBusHandlerResult get_modules(DBusConnection * connection,
+                              DBusMessage * message)
+{
+    DBusMessage *reply;
+    DBusMessageIter iter, iter1, iter2;
+    
+    IalModule *m = NULL;
+
+    reply = dbus_message_new_method_return(message);
+
+    if (reply == NULL) {
+        ERROR(("No memory."));
+        return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
+    }
+
+
+    dbus_message_iter_init(reply, &iter);
+    dbus_message_iter_append_array(&iter, &iter1, DBUS_TYPE_ARRAY);
+    m = modules_list_head;
+
+    while (m) {
+        dbus_message_iter_append_array(&iter1, &iter2, DBUS_TYPE_STRING);
+        
+        dbus_message_iter_append_string(&iter2, m->data->name);
+        dbus_message_iter_append_string(&iter2, m->data->token);
+        dbus_message_iter_append_string(&iter2, m->data->version); 
+        dbus_message_iter_append_string(&iter2, m->data->author);
+        dbus_message_iter_append_string(&iter2, m->data->descr);
+
+        switch (m->data->type) {
+        case INPUT:
+            dbus_message_iter_append_string(&iter2, "0");
+            break;
+            
+        case OUTPUT:
+            dbus_message_iter_append_string(&iter2, "1");
+            break;
+
+        case INPUT_OUTPUT:
+            dbus_message_iter_append_string(&iter2, "2");
+            break;
+
+        default:
+            dbus_message_iter_append_string(&iter2, "-1");
+            break;
+        }
+
+        if (m->data->initialized== TRUE)
+            dbus_message_iter_append_string(&iter2, "true");
+        else
+            dbus_message_iter_append_string(&iter2, "false");
+
+        if (m->data->state == TRUE)
+            dbus_message_iter_append_string(&iter2, "true");
+        else 
+            dbus_message_iter_append_string(&iter2, "false");
+
+        m = m->next;
+    }
+
+    if (!dbus_connection_send (connection, reply, NULL)) {
+        ERROR(("No memory."));
+        return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
+    }
+
+    dbus_message_unref(reply);
+    
+    return DBUS_HANDLER_RESULT_HANDLED;
+}
+
+DBusHandlerResult filter_function(DBusConnection * connection,
+                                  DBusMessage * message,
+                                  void *user_data)
+{
+    if (dbus_message_is_method_call(message, "com.novell.Ial", "GetModules") &&
+        strcmp(dbus_message_get_path(message), "/com/novell/Ial") == 0) {
+        
+        return get_modules(connection, message);
+    }
+    else {
+        return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
+    }
+
+    return DBUS_HANDLER_RESULT_HANDLED;
+}
+
+
+void iald_dbus_init(void)
+{
+    dbus_connection_add_filter(dbus_connection, filter_function, loop, NULL);
+
+    dbus_bus_add_match(dbus_connection,
+                       "type='signal',"
+                       "interface='"IAL_DBUS_INTERFACE_EVENT"',"
+                       "path='"IAL_DBUS_PATH_EVENT"',",
+                       NULL);
+}
