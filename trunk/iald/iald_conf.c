25352a49c9637a645678e6153a4c5753f155c481
thoenig <thoenig@8e7fc960-65e5-0310-a079-fea189f357c6>
2005-02-15 10:42:59 +0000

    copyright update

--- trunk/iald/iald_conf.c
+++ trunk/iald/iald_conf.c
@@ -1,7 +1,7 @@
 /* iald_conf.c - Input Abstraction Layer Configuration File Parser
  *
- * Copyright (C) 2004 Timo Hoenig <thoenig@nouse.net>
- *                    All rights reserved
+ * Copyright (C) 2004, 2005 Timo Hoenig <thoenig@nouse.net>
+ *                          All rights reserved
  *
  * Licensed under the Academic Free License version 2.1
  * 
